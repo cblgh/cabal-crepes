7b46b0e34e7be6f5e609e7d8e7ce38e76c3e6b3a
cblgh <cblgh@cblgh.org>
2019-06-21 13:42:43 +0200

    oops add site

---
+++ site/scripts/marionette.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", load)
+
+var commands = ["stat", "start", "stop", "connect", "disconnect"]
+
+function el (node) {
+    return document.createElement(node)
+}
+
+function load () {
+    commands.forEach(instantiate)
+    var rename = function () {
+        var puppetid = document.getElementById("puppet").value
+        var newName = document.getElementById("puppetname").value
+        POST({ url: `name/${puppetid}/${newName}`, cb: log})
+    }
+    createButton("name", rename)
+}
+
+function instantiate (cmd) {
+    var action = function () {
+        var puppetid = document.getElementById("puppet").value
+        POST({ url: `${cmd}/${puppetid}`, cb: log})
+    }
+    createButton(cmd, action)
+}
+
+function createButton (cmd, action) {
+    var button = el("button")
+    button.innerHTML = cmd
+    button.onclick = action
+    var controls = document.getElementById("controls")
+    controls.append(button)
+}
+
+
+function POST (opts) {
+    if (!opts.cb) opts.cb = noop
+    var fetchOptions =  {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    }
+    if (opts.data) { fetchOptions.body = JSON.stringify(opts.data) }
+    fetch(window.location + opts.url, fetchOptions)
+        .then(res => res.json()).then(opts.cb)
+        .catch((err) => {
+            log({ msg: `Error: ${opts.url} doesn't return json`})
+        })
+}
+
+function log (msg) {
+    var term = document.getElementById("terminal")
+    var time = new Date().toISOString().split("T")[1].split(".")[0]
+    var entry = el("div")
+    entry.innerHTML = `[${time}] ${msg.msg}`
+    term.append(entry)
+}
+
+function noop () {}
