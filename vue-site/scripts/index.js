efa42b1ddc976277f03953b16c5237d730ada8cf
Alexander Cobleigh <cblgh@cblgh.org>
2019-11-09 20:11:20 +0100

    client side mute in web version

--- vue-site/scripts/index.js
+++ vue-site/scripts/index.js
@@ -102,8 +102,8 @@ Vue.component("base-view", {
                             <button @click="togglePosting()">{{ curr.posting ? "stop posting" : "start posting" }}</button>
                             <button @click="shutdown()">shutdown</button>
                             <div class="spacer"></div>
-                            <button> mute </button>
-                            <select>
+                            <button @click="toggleMute()"> {{ currentMutes.includes(muteSelect) ? "unmute" : "mute" }} </button>
+                            <select v-model="muteSelect">
                                 <option v-for="id in everyoneButMe" :value="id"> {{ puppets[id].nick }}</option>
                             </select>
                             <div class="quarter-spacer"></div>
@@ -114,10 +114,18 @@ Vue.component("base-view", {
                             <select>
                                 <option v-for="val in [0.0, 0.25, 0.50, 0.80, 1.0]" :value="val"> {{ val }}</option>
                             </select>
+                            <div class="spacer"></div>
+                            <div>
+                                <h4> Mutes </h4>
+                                <div v-if="currentMutes.length === 0"><i> none </i></div> 
+                                <ul v-else>
+                                    <li v-for="mute in currentMutes"> {{ puppetNick(mute) }} by {{ puppetNick(currentPuppet) }} </li>
+                                </ul>
+                            </div>
                         </template>
                     </div>
                 </div>
-                <div class="log-panel">
+                <div class="log-panel" :class="{'debug-theme': debug}">
                     <button @click="sendCommand('spawn')">new puppet</button>
                     <button @click="debug = !debug"> {{ debug ? "chat" : "debug" }}</button>
                     <div v-show="debug" class="debug" :class="{'active-scroller': debug}">
@@ -126,7 +134,7 @@ Vue.component("base-view", {
                     <div v-show="!debug" class="chat" :class="{'active-scroller': !debug}">
                         <h3 v-if="currentPuppet.length > 0">{{ puppetNick(currentPuppet) }}:{{ currentPuppet.slice(0, 3) }}</h3>
                         <div id="chat">
-                            <div v-for="msg in chat[currentPuppet]">
+                            <div v-for="msg in chat[currentPuppet]" :class="{muted: isMuted(msg.author)}">
                                 {{ formatDate(msg.timestamp) }} <{{ puppetNick(msg.author) }}> {{ msg.message }}
                             </div>
                         </div>
@@ -138,7 +146,9 @@ Vue.component("base-view", {
     data () {
         return {
             rawlogs: [],
+            mutes: [], // list of { origin: <puppetid>, target: <puppetid> }
             chat: {},
+            muteSelect: "",
             debug: false,
             puppets: {},
             currentPuppet: ""
@@ -148,6 +158,9 @@ Vue.component("base-view", {
         curr () {
             if (!(this.currentPuppet in this.puppets)) return {}
             return this.puppets[this.currentPuppet]
+        },
+        currentMutes () {
+            return this.mutes.filter((m) => m.origin === this.currentPuppet).map((m) => m.target)
         },
         everyoneButMe () {
             let keys = Object.keys(this.puppets)
@@ -180,6 +193,18 @@ Vue.component("base-view", {
         })
     },
     methods: {
+        isMuted (puppetid) {
+            return this.currentMutes.includes(puppetid)
+        },
+        toggleMute () {
+            let isMuted = this.currentMutes.includes(this.muteSelect)
+            if (isMuted) {
+                let i = this.mutes.findIndex((m) => m.origin === this.currentPuppet && m.target === this.muteSelect)
+                this.mutes.splice(i, 1)
+            } else {
+                this.mutes.push({ origin: this.currentPuppet, target: this.muteSelect })
+            }
+        },
         toggleConnect () {
             let puppet = this.puppets[this.currentPuppet]
             let command = puppet.connected ? "disconnect" : "connect"
